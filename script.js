83304e1b7a7d4e4b6a3ce3b3a4e4c9ca58cc73b5
beamoraess <beatrizm.silva45@gmail.com>
2019-09-01 19:29:58 -0300

    adiciona teste pronto

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(function () {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 200) {
-       $("#navbar").addClass("navbar-effect");
-      } else {
-       $("#navbar").removeClass("navbar-effect");
-      }
-    });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $("#navbar").addClass("navbar-effect");
+    } else {
+      $("#navbar").removeClass("navbar-effect");
+    }
   });
+});
